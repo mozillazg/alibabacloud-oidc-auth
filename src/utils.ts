f1f004ac086b82e1f676e7bfdebb134d4840ebca
mozillazg <mozillazg101@gmail.com>
2023-01-30 21:13:52 +0800

    support export new environment variable: ALICLOUD_SECURITY_TOKEN
    closes #8

--- src/utils.ts
+++ src/utils.ts
@@ -18,11 +18,11 @@ export function exportEnvs(accessKeyId: string, accessKeySecret: string, securit
     core.exportVariable('ALIBABACLOUD_ACCESS_KEY_ID', accessKeyId);
     core.exportVariable('ALIBABA_CLOUD_ACCESS_KEY_SECRET', accessKeyId);
     core.exportVariable('ALICLOUD_SECRET_KEY', accessKeySecret);
-    core.exportVariable('ALICLOUD_SECRET_KEY', accessKeySecret);
     core.exportVariable('ALIBABACLOUD_ACCESS_KEY_SECRET', accessKeySecret);
     core.exportVariable('ALIBABA_CLOUD_SECURITY_TOKEN', securityToken);
     core.exportVariable('ALICLOUD_ACCESS_KEY_STS_TOKEN', securityToken);
     core.exportVariable('ALIBABACLOUD_SECURITY_TOKEN', securityToken);
+    core.exportVariable('ALICLOUD_SECURITY_TOKEN', securityToken);
 }
 
 export function setOutputs(accessKeyId: string, accessKeySecret: string, securityToken: string) {
